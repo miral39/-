2ab99faee33ae6cfc7ca597c7099b8fbfa2f161e
miral39 <91869568+miral39@users.noreply.github.com>
2022-03-09 22:01:26 +0800

    Add files via upload

---
+++ 青蛙.java
@@ -0,0 +1,14 @@
+public class 青蛙 {
+    public static void main(String[] args) {
+    int n=10;
+    int[] arr= new int[n];
+    arr[0]=1;
+    arr[1]=2;
+    for(int i=2;i<n;i++)
+    {arr[i]=arr[i-1]+arr[i-2];}
+        System.out.println("第");
+    }
+}
+
+
+
